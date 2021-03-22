287b5b4c1d9736573dc4b01c322f1e002e974e3b
Aakash-rai1 <57335739+Aakash-rai1@users.noreply.github.com>
2021-03-22 15:40:41 +0545

    added upload middleware

---
+++ middleware/upload.js
@@ -0,0 +1,28 @@
+const multer = require('multer');
+ 
+const storage = multer.diskStorage({
+    destination :function(req, file, cb) {
+cb(null, './media/images')
+    },
+ 
+    filename : function(req, file, cb){
+        cb(null,Date.now() + file.originalname)
+    }
+});
+ 
+const fileFilter = function(req, file, cb){
+    if(file.mimetype =='image/png' || file.mimetype== 'image/jpeg'){
+        cb(null,true)
+    }
+    else{
+        cb(null, false)
+    }
+}
+ 
+const upload = multer({
+    storage : storage, 
+    fileFilter : fileFilter ,
+    limits :{fileSize : 8000000}
+});
+ 
+module.exports = upload;
