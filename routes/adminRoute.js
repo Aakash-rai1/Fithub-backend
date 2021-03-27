9436c0f9a39cf8ae0e3f99c11999beecc8156c98
Aakash-rai1 <57335739+Aakash-rai1@users.noreply.github.com>
2021-03-27 19:37:19 +0545

    added admin route

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,16 +1,57 @@
-const express = require("express");
+const express = require('express'); //third party
+const admin = require('../models/adminModel');
 const router = express.Router();
-const userController = require("../controller/userController")
-const auth = require('../middleware/auth')
+
+const bcrypt = require('bcryptjs')
+const {check, validationResult} = require('express-validator');
+const { request } = require('http');
+const { resolveSoa } = require('dns');
+const jwt = require('jsonwebtoken')
+
+router.post('/admin/add',
+[
+    check('email', "Invalid Emial Address ").isEmail(),
+    check('fname', "You must enter fname").not().isEmpty()
+
+],
+ function(req, res){
+    const  errors = validationResult(req);
+    console.log(errors.array())
+
+    //is empty
+    if(!errors.isEmpty()){
+        res.status(400).json(errors.array())
+    }
+    else{
+
+        const fname = req.body.fname
+        const lname = req.body.lname
+        const email = req.body.email
+        const password = req.body.password
 
 
-router.post("/adminlogin", userController.login)
-router.get("/admincheckLogin",auth,userController.checklogin)
-router.post("/createAdmin",userController.addUser)
-router.get("/findAdmin",userController.findUser)
-router.get("/findAdminById/:_id",userController.findUserById)
-router.delete("/deleteAdmin/:_id",userController.deleteUserById)
-router.put("/updateAdmin/:_id",auth,userController.updateUser)
-router.get("/getAdminbyemail/:email",userController.checkemail)
-router.delete("/logoutAdmin",auth,userController.logout)
-module.exports = router
+
+
+        bcrypt.hash(password,10, function(err, hash){
+            console.log(hash)
+            const me = new admin({fname : fname, lname : lname, email:email, password:hash});
+            me.save()
+            .then(function(result){
+                // success insert
+                res.status(201).json({success: true,
+                    message : "Admin Registered success"});
+                    console.log('success')
+
+            })
+            .catch(function(err){
+                res.status(500).json({success: false,
+                    message : err})
+            });
+            console.log("Sucessfully Registered");
+        })
+
+    }
+
+})
+
+module.exports = router;
